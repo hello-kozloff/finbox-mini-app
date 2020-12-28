ee8adf10f4f80629754201fdb3b0dfb1fb27eeb0
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 04:06:34 +0300

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-export { default as Dashboard } from './Dashboard';
-export { default as Contacts } from './Contacts';
+export { default as Dashboard } from './Dashboard/Dashboard';
+export { default as Contacts } from './Contacts/Contacts';
