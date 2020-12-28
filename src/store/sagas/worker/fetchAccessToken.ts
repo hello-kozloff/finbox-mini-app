924357aec6330c4755ba25f8f85ea720bcf745f0
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 21:14:31 +0300

    change app id

--- src/store/sagas/worker/fetchAccessToken.ts
+++ src/store/sagas/worker/fetchAccessToken.ts
@@ -2,7 +2,7 @@ import bridge from '@vkontakte/vk-bridge';
 
 export function* fetchAccessTokenWorker() {
   const response = yield bridge.send('VKWebAppGetAuthToken', {
-    'app_id': 9999,
+    'app_id': 7712603,
     'scope': 'friends'
   });
 
