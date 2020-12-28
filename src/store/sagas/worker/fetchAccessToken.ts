270fd89fc023f41ca77b7b6ed622bacb28cf2086
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 21:21:32 +0300

    push access token in store

--- src/store/sagas/worker/fetchAccessToken.ts
+++ src/store/sagas/worker/fetchAccessToken.ts
@@ -1,11 +1,22 @@
+import { put } from 'redux-saga/effects';
 import bridge from '@vkontakte/vk-bridge';
+import { ISetUserAction } from '../../actions/user/types';
+import { SET_USER } from '../../constants/user';
 
 export function* fetchAccessTokenWorker() {
   const response = yield bridge.send('VKWebAppGetAuthToken', {
     'app_id': 7712603,
     'scope': 'friends'
+  }).catch(() => {
+    alert('Прроизошло критическая ошибка. Пожалуйста, перезагрузите приложение.')
   });
 
-  console.log('We revive data!')
-  console.log(response);
+  yield put<ISetUserAction>({
+    type: SET_USER,
+    payload: {
+      user: {
+        access_token: response.access_token
+      }
+    }
+  });
 }
