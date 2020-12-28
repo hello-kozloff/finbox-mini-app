bbb730634b21465dcd09cbddd07139330d720b0a
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 20:57:01 +0300

    connect with saga

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers';
+import watcher from './sagas/watcher';
 import { createBrowserHistory } from 'history';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger)));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger, sagaMiddleware)));
 const history = createBrowserHistory();
+
+sagaMiddleware.run(watcher);
 
 export { store, history };
