8d3b2f08d8ab46adc3b87e55f5255f2e0bafc22b
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 03:30:39 +0300

    add catalog panel

---
+++ src/panels/Catalog/Catalog.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Panel, PanelHeader } from '@vkontakte/vkui';
+import { PanelProps } from '@vkontakte/vkui/dist/components/Panel/Panel';
+
+/**
+ * The catalog panel.
+ *
+ * @param props
+ * @constructor
+ */
+export default function CatalogPanel(props: PanelProps): React.ReactElement {
+  return (
+    <Panel id={props.id}>
+      <PanelHeader>Catalog</PanelHeader>
+    </Panel>
+  );
+}
