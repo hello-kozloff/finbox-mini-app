22becd579d2cfdb0e22a0d6846305c1c9921d580
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 21:12:00 +0300

    add logic

--- src/store/sagas/worker/fetchAccessToken.ts
+++ src/store/sagas/worker/fetchAccessToken.ts
@@ -1,3 +1,11 @@
+import bridge from '@vkontakte/vk-bridge';
+
 export function* fetchAccessTokenWorker() {
-  console.log('hello world!');
+  const response = yield bridge.send('VKWebAppGetAuthToken', {
+    'app_id': 9999,
+    'scope': 'friends'
+  });
+
+  console.log('We revive data!')
+  console.log(response);
 }
