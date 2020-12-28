307f3e8b1aa988cb4b4d9e7b651e2dde130aeda1
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-28 03:20:53 +0300

    create example tabbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { AppRoot, View, Panel, PanelHeader, Header, Group, Cell } from '@vkontakte/vkui';
+import { AppRoot, Epic, Tabbar, TabbarItem, View, Panel, PanelHeader } from '@vkontakte/vkui';
+import { Icon28NewsfeedOutline } from '@vkontakte/icons';
+import { IStory } from './types/story';
 
 /**
  * The app component.
@@ -7,17 +9,30 @@ import { AppRoot, View, Panel, PanelHeader, Header, Group, Cell } from '@vkontak
  * @constructor
  */
 export default function App(): React.ReactElement {
+  const [activeStory, setActiveStory] = React.useState<IStory>('feed');
+
+  function onStoryChange(e: any): void {
+    return setActiveStory(e.currentTarget.dataset.story);
+  }
+
   return (
     <AppRoot>
-      <View activePanel="main">
-        <Panel id="main">
-          <PanelHeader>VKUI</PanelHeader>
-          <Group header={<Header mode="secondary">Items</Header>}>
-            <Cell>Hello</Cell>
-            <Cell>World</Cell>
-          </Group>
-        </Panel>
-      </View>
+      <Epic activeStory={activeStory} tabbar={
+        <Tabbar>
+          <TabbarItem
+            onClick={onStoryChange}
+            selected={activeStory === 'feed'}
+            data-story="feed"
+            text="Новости"
+          ><Icon28NewsfeedOutline /></TabbarItem>
+        </Tabbar>
+      }>
+        <View id="feed" activePanel="feed">
+          <Panel id="feed">
+            <PanelHeader>Feed</PanelHeader>
+          </Panel>
+        </View>
+      </Epic>
     </AppRoot>
   );
 }
