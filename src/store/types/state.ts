92fa5d8f345064a468d58f27ca9139dae6753651
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-29 20:01:26 +0300

    update state interface

--- src/store/types/state.ts
+++ src/store/types/state.ts
@@ -1,8 +1,10 @@
 import { IUserState } from '../reducers/user/types';
+import { IFriendsState } from "../reducers/friends/types";
 
 /**
  * The state interface.
  */
 export interface IState {
   user: IUserState;
+  friends: IFriendsState;
 }
