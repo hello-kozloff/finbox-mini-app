3b735af3885662feafb775f6cbbb54705ee8736a
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-29 18:32:16 +0300

    render informer component

--- src/panels/App/App.tsx
+++ src/panels/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Panel, PanelHeader } from '@vkontakte/vkui';
 import { PanelProps } from '@vkontakte/vkui/dist/components/Panel/Panel';
-import { Contacts, Dashboard } from '../../components';
+import { Informer, Contacts, Dashboard } from '../../components';
 
 /**
  * The app panel.
@@ -13,6 +13,7 @@ export default function AppPanel(props: PanelProps): React.ReactElement {
   return (
     <Panel id={props.id}>
       <PanelHeader>Долги</PanelHeader>
+      <Informer />
       <Dashboard />
       <Contacts />
     </Panel>
