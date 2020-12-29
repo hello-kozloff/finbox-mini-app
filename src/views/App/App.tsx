e6169a8b22aa068b9230029e012a14692286ce2d
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-29 18:16:23 +0300

    use types

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View } from '@vkontakte/vkui';
 import { AppPanel } from "../../panels";
+import { ViewProps } from "@vkontakte/vkui/dist/components/View/View";
+import { PanelProps } from "@vkontakte/vkui/dist/components/Panel/Panel";
 
 /**
  * The app view.
  *
  * @constructor
  */
-export default function AppView(props: { id: string, activePanel: string }): React.ReactElement {
+export default function AppView(props: ViewProps & PanelProps): React.ReactElement {
   return (
     <View id={props.id} activePanel={props.activePanel}>
       <AppPanel id={props.id} />
--- src/views/Catalog/Catalog.tsx
+++ src/views/Catalog/Catalog.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View } from '@vkontakte/vkui';
 import { CatalogPanel } from "../../panels";
+import { ViewProps } from "@vkontakte/vkui/dist/components/View/View";
+import { PanelProps } from "@vkontakte/vkui/dist/components/Panel/Panel";
 
 /**
  * The catalog view.
  *
  * @constructor
  */
-export default function CatalogView(props: { id: string, activePanel: string }): React.ReactElement {
+export default function CatalogView(props: ViewProps & PanelProps): React.ReactElement {
   return (
     <View id={props.id} activePanel={props.activePanel}>
       <CatalogPanel id={props.id} />
