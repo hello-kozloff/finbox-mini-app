8a751ce1163c0af902ca6e3c6c63aab70ca5ccdf
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-29 20:48:54 +0300

    add firebase config

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyCumX0HBTrTWuiyCOkwfSmI_ro5pGEGCzM",
+  authDomain: "finbox-mini-app.firebaseapp.com",
+  databaseURL: "https://finbox-mini-app-default-rtdb.firebaseio.com",
+  projectId: "finbox-mini-app",
+  storageBucket: "finbox-mini-app.appspot.com",
+  messagingSenderId: "261332152446",
+  appId: "1:261332152446:web:1b0638cbb10e860a86a82f"
+};
+
+export default config;
