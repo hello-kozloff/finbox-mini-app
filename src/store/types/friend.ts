dc2ff48471890c921aa113f8522720f683a47ebf
Nikita Kozloff <hello.kozloff@gmail.com>
2020-12-29 19:42:02 +0300

    add friend interface

---
+++ src/store/types/friend.ts
@@ -0,0 +1,11 @@
+/**
+ * The friend interface.
+ */
+export default interface IFriend {
+  id: number;
+  first_name: string;
+  last_name: string;
+  nickname: string;
+  photo_100: string;
+  track_code: string;
+}
