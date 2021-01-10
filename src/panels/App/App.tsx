5aa03a1fd8ab284d835783e8bafd5483e2808d57
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-10 16:27:41 +0300

    fix

--- src/panels/App/App.tsx
+++ src/panels/App/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Panel, PanelHeader } from '@vkontakte/vkui';
-import { ContentFix, Informer, Dashboard } from '../../components';
+import { ContentFix, Dashboard } from '../../components';
 import IPanelProps from "../../types/panelProps";
 
 /**
@@ -13,7 +13,6 @@ export default function AppPanel(props: IPanelProps): React.ReactElement {
   return (
     <Panel id={props.id}>
       <PanelHeader>Долги</PanelHeader>
-      {/*<Informer onModalShow={props.onShowModal} />*/}
       <ContentFix>
         <Dashboard />
       </ContentFix>
