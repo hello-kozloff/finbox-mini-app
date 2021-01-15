d1bd2ac8f8f06bfb3a99f4a2bb0bd16f4def500e
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 08:58:46 +0300

    fix type

--- src/components/DebtCard/types.ts
+++ src/components/DebtCard/types.ts
@@ -1,12 +1,11 @@
 import IFriend from "../../store/types/friend";
 import {IAddDebtValues} from "../../modals/AddDebt";
-import { Moment } from 'moment';
 
 export interface IDebtCardProps {
   first_name: IFriend['first_name'];
   last_name: IFriend['last_name'];
   photo_100: IFriend['photo_100'];
   sum: IAddDebtValues['sum'];
-  createdAt: Moment;
+  createdAt: string;
   expirationDate: IAddDebtValues['expirationDate'];
 }
