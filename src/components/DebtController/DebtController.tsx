84b59dbd4674cc1faade423ed9a092b614afeaa0
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 07:29:27 +0300

    upgrade render

--- src/components/DebtController/DebtController.tsx
+++ src/components/DebtController/DebtController.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { block } from "bem-cn";
+import { FirebaseDatabaseNode } from "@react-firebase/database";
+import { Spinner } from '@vkontakte/vkui';
 import { DebtCarousel } from './modules';
+import { getCurrentUserId } from "../../utils";
+
+const debtContainer = block('debt-container');
 
 export default function DebtController(): React.ReactElement {
   const [index, setIndex] = React.useState<number>(0);
@@ -8,7 +14,21 @@ export default function DebtController(): React.ReactElement {
 
   return (
     <div>
-      <DebtCarousel onChange={(index) => setIndex(index)} />
+      <DebtCarousel
+        onChange={(index) => setIndex(index)}
+      />
+      <div className={debtContainer()}>
+        <span className={debtContainer('header')}>
+          Займы по <button type="button">большей сумме ₽</button>
+        </span>
+        <div className={debtContainer('content')}>
+          <FirebaseDatabaseNode path={getCurrentUserId() || '/'}>
+            {(data) => (
+              data.isLoading ? <Spinner size="regular" /> : <div>Hello</div>
+            )}
+          </FirebaseDatabaseNode>
+        </div>
+      </div>
     </div>
   );
 }
