2d2cff5cfbc4d2bef3e2a0ff70251cf79d6bb2e3
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 07:16:16 +0300

    upgrade component

--- src/components/DebtController/modules/Carousel/Carousel.tsx
+++ src/components/DebtController/modules/Carousel/Carousel.tsx
@@ -6,57 +6,68 @@ import { Spinner } from '@vkontakte/vkui';
 import { getCurrentUserId } from "../../../../utils";
 import { DashboardCard } from '../../../Dashboard/modules';
 import { DebtType } from "../../../../modals/AddDebt/types";
+import IDebtCarousel from './types';
 
-export default function DebtCarousel(): React.ReactElement {
-  const options: FlickityOptions = {
-    cellAlign: 'left',
-    prevNextButtons: false,
-    pageDots: false
-  };
+export default class DebtCarousel extends React.Component<IDebtCarousel> {
+  public instance: Flickity | undefined;
 
-  /**
-   * The function render total value.
-   * @param type
-   * @param data
-   */
-  function getTotalValue(type: DebtType, data: FirebaseDatabaseNodeChildFunctionProps) {
-    let sum = 0;
-
-    if (!data.isLoading && data.value !== null) {
-      Object.values(data.value).forEach((node: any) => {
-        console.log(node);
-        if (node.type === type) {
-          console.log(node);
-          sum = Number(node.sum) + sum;
-        }
-      });
-    }
-
-    return `${sum} ₽`;
+  componentDidMount() {
+    this.instance?.on('change', (index: number) => {
+      this.props.onChange && this.props.onChange(index);
+    });
   }
 
-  return (
-    <Flickity options={options}>
-      <div className="carousel-cell">
-        <FirebaseDatabaseNode path={getCurrentUserId() || '/'}>
-          {(data) => (
-            <DashboardCard
-              title="Полученные займы"
-              subtitle={data.isLoading ? <Spinner size="regular" /> : getTotalValue(DebtType.borrowed, data)}
-            />
-          )}
-        </FirebaseDatabaseNode>
-      </div>
-      <div className="carousel-cell">
-        <FirebaseDatabaseNode path={getCurrentUserId() || '/'}>
-          {(data) => (
-            <DashboardCard
-              title="Выданные займы"
-              subtitle={data.isLoading ? <Spinner size="regular" /> : getTotalValue(DebtType.lent, data)}
-            />
-          )}
-        </FirebaseDatabaseNode>
-      </div>
-    </Flickity>
-  );
+  render() {
+    const options: FlickityOptions = {
+      initialIndex: this.props.initialIndex || 0,
+      cellAlign: 'left',
+      prevNextButtons: false,
+      pageDots: false
+    };
+
+    /**
+     * The function render total value.
+     * @param type
+     * @param data
+     */
+    function getTotalValue(type: DebtType, data: FirebaseDatabaseNodeChildFunctionProps) {
+      let sum = 0;
+
+      if (!data.isLoading && data.value !== null) {
+        Object.values(data.value).forEach((node: any) => {
+          console.log(node);
+          if (node.type === type) {
+            sum = Number(node.sum) + sum;
+          }
+        });
+      }
+
+      return `${sum} ₽`;
+    }
+
+    return (
+      <Flickity flickityRef={(ref) => this.instance = ref} options={options}>
+        <div className="carousel-cell">
+          <FirebaseDatabaseNode path={getCurrentUserId() || '/'}>
+            {(data) => (
+              <DashboardCard
+                title="Полученные займы"
+                subtitle={data.isLoading ? <Spinner size="regular" /> : getTotalValue(DebtType.borrowed, data)}
+              />
+            )}
+          </FirebaseDatabaseNode>
+        </div>
+        <div className="carousel-cell">
+          <FirebaseDatabaseNode path={getCurrentUserId() || '/'}>
+            {(data) => (
+              <DashboardCard
+                title="Выданные займы"
+                subtitle={data.isLoading ? <Spinner size="regular" /> : getTotalValue(DebtType.lent, data)}
+              />
+            )}
+          </FirebaseDatabaseNode>
+        </div>
+      </Flickity>
+    );
+  }
 }
