7bbb34225557c832637eb727d3c7a31f0e58cb84
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 03:25:38 +0300

    export modal

---
+++ src/modals/AddDebt/index.ts
@@ -0,0 +1,2 @@
+export { default as AddDebtModal } from './AddDebt';
+export type { IAddDebtValues } from './types';
---
+++ src/modals/index.ts
@@ -0,0 +1,2 @@
+export { default as AddDebtModal } from './AddDebt/AddDebt';
+export type { IAddDebtValues } from './AddDebt/types';
