4119249530253b31cfcad5b84845b5729899127c
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 07:19:48 +0300

    use state

--- src/components/DebtController/DebtController.tsx
+++ src/components/DebtController/DebtController.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { DebtCarousel } from './modules';
 
 export default function DebtController(): React.ReactElement {
+  const [index, setIndex] = React.useState<number>(0);
+
+  console.log(index);
+
   return (
     <div>
-      <DebtCarousel />
+      <DebtCarousel onChange={(index) => setIndex(index)} />
     </div>
   );
 }
