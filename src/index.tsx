0a3dadd6c35208347853d29bc2f62f63d61837e9
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 12:14:40 +0300

    some features

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
+import "core-js/features/map";
+import "core-js/features/set";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './styles/index.css';
+import bridge from "@vkontakte/vk-bridge";
+
+bridge.send("VKWebAppInit");
 
 ReactDOM.render(<App />, document.getElementById('root'));
+if (process.env.NODE_ENV === "development") {
+  import("./eruda").then(({ default: eruda }) => {}); //runtime download
+}
