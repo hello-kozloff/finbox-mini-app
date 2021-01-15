a958cba7ab6065b6e3c83579666875789276aaba
Nikita Kozloff <hello.kozloff@gmail.com>
2021-01-15 04:28:40 +0300

    use dynamicContentHeight

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -35,7 +35,7 @@ function AppView(props: ViewProps & PanelProps & { friends: IFriendsState }): Re
 
   const modal = (
     <ModalRoot activeModal={activeModal} onClose={onCancelModal}>
-      <AddDebtModal id="add-debt" />
+      <AddDebtModal id="add-debt" dynamicContentHeight />
     </ModalRoot>
   );
 
